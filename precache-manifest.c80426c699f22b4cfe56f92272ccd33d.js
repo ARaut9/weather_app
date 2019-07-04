9fe955b29240267c1f738a5a19213db4fef64093
ARaut9 <ankitraut999@gmail.com>
2019-07-04 17:32:23 +0530

    Updates

---
+++ precache-manifest.c80426c699f22b4cfe56f92272ccd33d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748fb833dabfd874ec859a742d1e8db8",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "7cffe1ff9348e4a954a0",
+    "url": "/weather_app/static/css/main.8c4c8210.chunk.css"
+  },
+  {
+    "revision": "f5fe11c915f92668079e",
+    "url": "/weather_app/static/js/2.f105d4e9.chunk.js"
+  },
+  {
+    "revision": "7cffe1ff9348e4a954a0",
+    "url": "/weather_app/static/js/main.f336cd9a.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  }
+]);
